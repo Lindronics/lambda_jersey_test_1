0633a814184b08c9ffd9dbab8a71ab09fa859ea0
Niklas <niklas@lindorfer.com>
2021-02-02 16:29:14 +0000

    Using gson for logging

--- src/main/java/com/lindronics/StreamLambdaHandler.java
+++ src/main/java/com/lindronics/StreamLambdaHandler.java
@@ -4,9 +4,11 @@ import com.amazonaws.serverless.proxy.jersey.JerseyLambdaContainerHandler;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -24,10 +26,15 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     private static final JerseyLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler
             = JerseyLambdaContainerHandler.getAwsProxyHandler(jerseyApplication);
 
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
             throws IOException {
-        context.getLogger().log((new ObjectMapper()).writeValueAsString(context));
+
+        LambdaLogger logger = context.getLogger();
+        logger.log("CONTEXT: " + gson.toJson(context));
+
         handler.proxyStream(inputStream, outputStream, context);
     }
 }
