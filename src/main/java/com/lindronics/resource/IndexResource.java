956710f8330f20a79057c095a872f36e61fa05d5
Niklas <niklas@lindorfer.com>
2021-02-03 11:33:24 +0000

    Minor change to index service

--- src/main/java/com/lindronics/resource/IndexResource.java
+++ src/main/java/com/lindronics/resource/IndexResource.java
@@ -20,7 +20,7 @@ public class IndexResource {
     @Consumes(MediaType.WILDCARD)
     public Response ping() {
         Map<String, String> pong = new HashMap<>();
-        pong.put("pong", "Hello, World!");
+        pong.put("test", "Hello, hello, hello!");
         return Response.status(200).entity(pong).build();
     }
 }
